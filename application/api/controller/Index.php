f05704c98df1e400655525ced89e83194f95e606
chaihongjun <441315628@qq.com>
2020-06-15 17:19:10 +0800

    update
    update

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -56,23 +56,15 @@ class Index extends Controller
         $Blogs['cateName'] =  $cateName;
         $Blogs['cateDir'] =  $cateDir;
         $blogs['categoryID'] = $categoryID;
-        // echo "1";
-        // return;
       }
       //【栏目ID】存在【分页】不存在
       else {
         // 【栏目ID】存在【分页】不存在【limit】不存在
         if (empty($limit)) {
           $data =  collection(ArticleModel::where('cid', $categoryID)->select())->toArray();
-          // [
-          //  
-          //
-          //
-          // ]
+
           $total = count($data);
-          // $per_page = $pagesize;
-          // $current_page = null;
-          // $last_page = null;
+
           $cateName =  CategoryModel::where('id', $categoryID)->value('name');
           $cateDir = CategoryModel::where('name', $cateName)->value('dir');
           //添加 分类名称 分类目录 分类ID到结果
@@ -81,21 +73,13 @@ class Index extends Controller
           $Blogs['cateDir'] =  $cateDir;
           $blogs['categoryID'] = $categoryID;
           $Blogs['data'] =   $data;
-          // echo "2";
-          // return;
         }
         // 【栏目ID】  存在   【分页】   不存在 【limit】  存在
         else {
           $data =  collection(ArticleModel::where('cid', $categoryID)->limit($limit)->select())->toArray();
-          // [
-          //  
-          //
-          //
-          // ]
+
           $total = count($data);
-          // $per_page = $pagesize;
-          // $current_page = null;
-          // $last_page = null;
+
           $cateName =  CategoryModel::where('id', $categoryID)->value('name');
           $cateDir = CategoryModel::where('name', $cateName)->value('dir');
           //添加 分类名称 分类目录 分类ID到结果
@@ -104,8 +88,6 @@ class Index extends Controller
           $Blogs['cateDir'] =  $cateDir;
           $blogs['categoryID'] = $categoryID;
           $Blogs['data'] =   $data;
-          // echo "3";
-          // return;
         }
       }
     }
@@ -116,93 +98,30 @@ class Index extends Controller
         $Blogs = ArticleModel::paginate($pagesize, false, ['page' => $pageNumber])->toArray();
         //分页查询文章对应的 栏目ID
         $articleCateID = ArticleModel::where('id', '>', 0)->field('cid')->paginate($pagesize, false, ['page' => $pageNumber])->toArray();
-        // 0 => 
-        //   array (size=1)
-        //     'cid' => int 1
-        // 1 => 
-        //   array (size=1)
-        //     'cid' => int 4
-        // 2 => 
-        //   array (size=1)
-        //     'cid' => int 1
-        // 3 => 
-        //   array (size=1)
-        //     'cid' => int 4
-        // var_dump($cateIDPerPage);
-        // exit;
-        // $cateIDPerPage[0]['cid']  获取当前页面第一个文章的栏目ID
-        // 获取全部栏目名称和对应ID
-        //$cateDirPerPage = CategoryModel::column('name, dir', 'id');
-
-        // array (size=5)
-        // 1 => 
-        //   array (size=3)
-        //     'id' => int 1
-        //     'name' => string '科技' (length=6)
-        //     'dir' => string '/tech' (length=5)
-        // 2 => 
-        //   array (size=3)
-        //     'id' => int 2
-        //     'name' => string '产品中心' (length=12)
-        //     'dir' => string '/product' (length=8)
-        // 3 => 
-        //   array (size=3)
-        //     'id' => int 3
-        //     'name' => string '电脑' (length=6)
-        //     'dir' => string '/product/computer' (length=17)
-        // 4 => 
-        //   array (size=3)
-        //     'id' => int 4
-        //     'name' => string 'pc机' (length=5)
-        //     'dir' => string '/product/computer/pc' (length=20)
-        // 5 => 
-        //   array (size=3)
-        //     'id' => int 5
-        //     'name' => string '网站公告' (length=12)
-        //     'dir' => string '/notice' (length=7)
-
-
-        //  $cateDirPerPage[1]['id']  $cateDirPerPage[1]['name']  $cateDirPerPage[1]['dir']
-        // var_dump($cateDirPerPage);
-        // exit;
       }
       // 【栏目ID】 不存在  【分页】 不存在
       else {
         // 【栏目ID】 不存在  【分页】 不存在  【Limit】 存在
         if (!empty($limit)) {
           $data = collection(ArticleModel::limit($limit)->select())->toArray();
-          // [
-          //  
-          //
-          //
-          // ]
+
           $total = count($data);
-          // $per_page = $pagesize;
-          // $current_page = null;
-          // $last_page = null;
+
           $Blogs['total'] =   $total;
           $Blogs['data'] =   $data;
           $per_page = null;  //清空分页
-          // echo "5";
-          // return;
+
         }
         // 【栏目ID】 不存在  【分页】 不存在  【Limit】 不存在
         else {
           $data = collection(ArticleModel::all())->toArray();
-          // [
-          //  
-          //
-          //
-          // ]
+
           $total = count($data);
-          // $per_page = $pagesize;
-          // $current_page = null;
-          // $last_page = null;
+
           $Blogs['total'] =   $total;
           $Blogs['data'] =   $data;
           $per_page = null;  //清空分页 
-          // echo "6";
-          // return;
+
         }
       }
     }
@@ -244,11 +163,7 @@ class Index extends Controller
   {
     $categoryID = $request->param('categoryID');  //获取访问的栏目ID
     $limit = $request->param('limit');
-    // echo  '$categoryID: ' . $categoryID;
-    // echo '</br>';
-    // echo    '$limit: ' . $limit;
-    // var_dump($categoryID);
-    // return;
+
     //获取N条随机数
     $data =  collection(ArticleModel::randQuery($categoryID,  $limit))->toArray();
 
@@ -299,24 +214,7 @@ class Index extends Controller
   {
     $detailID = $request->param('detailID');  //获取指定内容ID
     $data = ArticleModel::get($detailID)->toArray();
-    // {
-    //   "id": 1,
-    //   "cid": 1,
-    //   "title": "云适配获千万美金B轮融资，将开发移动端企业安全浏览器",
-    //   "shorttitle": "云适配获千万美金B轮融资",
-    //   "introduction": "不论是打车，买菜，交水电费，乃至于按摩，现在几乎你能想到的消费行为都能通过移动端进行操作——事实已经证明移动端的对消费者服务是个大市场。那么对企业移动端的服务的市场又怎样呢？至少近日晨兴创投和 IDG 资本对云适配千万美元级的投资能告诉我们，风险资本觉得这个市场正在变好。",
-    //   "content": "<p><img src=\"https://demo.themebetter.com/dux/wp-content/uploads/sites/3/2015/06/110.jpg\"/></p><p>不论是打车，买菜，交水电费，乃至于按摩，现在几乎你能想到的消费行为都能通过移动端进行操作——事实已经证明移动端的对消费者服务是个大市场。那么对企业移动端的服务的市场又怎样呢？至少近日晨兴创投和 IDG 资本对云适配千万美元级的投资能告诉我们，风险资本觉得这个市场正在变好。</p><p>云适配创立于 2012 年，主要业务是通过网页内容的抓取和适配，将PC端的网页自动转化为适合移动端的网页。通过这项名为跨屏云 Xcloud 的服务，企业用户不用修改任何网页的前端设计便能把让本不支持移动端浏览的网页获得适应移动端排版和布局。 三年来，云适配的技术被用在了超过 30 万的企业网站上，其中包括中国政府网，北京大学官网，联想海信的官网等。<br/></p><p>大部分企业用户的网页再适配都是在公有云完成，这部分服务云适配免费提供。不过一些对信息安全极为重视的客户，比如政府和金融企业等，会选择将跨屏云部署在自己的私有云上，云适配会向这些用户收取一定的费用。<br/></p><p>除了网页适配外，云适配的另一项服务是开源的 HTML 5 前端框架 Amaze UI。Amaze UI 是一款类似 Bootsrap 的前端模块库，相较于 Twitter 驰名天下的 Boostrap，Amaze UI 做了更多针对中文和国内主流浏览器的优化。目前Amaze UI 在 GitHub上 有 4000 多的收藏和 1000 多的fork。利用 Amaze UI，开发者能使用和修改现成的 HTML 5 模块，从而加速开发。<br/></p><p>当然，一个仅仅是现金流良好的自动化网页适配公司并不需要千万美元级的 B轮融资。云适配正在开发一款移动安全浏览器，试图利用跨屏云带来的用户基础进入更深层的企业服务市场。这款安全浏览器支持缓存数据沙盒化，以及VPN 网络沙盒化等功能，从而在网页端提供一个安全的移动互联网办公环境。目前这款浏览器尚在开发中，预计今年九月发布。<br/></p><p>对于这次的融资，云适配的创始人，前微软 IE 工程师陈本峰说道，“因为我自己是 HTML 5 的中国区布道师，所以我觉得我们这次融资不仅是对这个企业，也是市场对 HTML 5 在更宽广的应用前景上的认可。”<br/></p><p><br/></p>",
-    //   "author": "admin",
-    //   "status": 1,
-    //   "reading": 0,
-    //   "thumb": "",
-    //   "photo": false,
-    //   "is_top": 0,
-    //   "is_recommend": 0,
-    //   "sort": 0,
-    //   "create_time": "2017-04-11 14:10:10",
-    //   "publish_time": "2017-04-11 14:09:45"
-    //   }
+
     $cateID = $data['cid'];
     $cateDir =  CategoryModel::where('id', $cateID)->value('dir');
     $cateName =  CategoryModel::where('id', $cateID)->value('name');
--- runtime/cache/a6/47c3e9699ec0be2d4eed3d9f78a26b.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:7:{s:10:"site_title";s:18:"后台管理系统";s:9:"seo_title";s:9:"SEO标题";s:11:"seo_keyword";s:12:"SEO关键字";s:15:"seo_description";s:9:"SEO说明";s:14:"site_copyright";s:0:"";s:8:"site_icp";s:14:"琼ICP18000000";s:11:"site_tongji";s:0:"";}
