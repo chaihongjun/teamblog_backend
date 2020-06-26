2c58f4eea1d81c004854ef16b94e23438facb755
chaihongjun <441315628@qq.com>
2020-06-26 17:28:39 +0800

    删除空行
    删除空行

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\admin\controller;
 
 use app\common\model\Article as ArticleModel;
@@ -12,151 +13,138 @@ use app\common\controller\AdminBase;
  */
 class Article extends AdminBase
 {
-    protected $article_model;
-    protected $category_model;
+  protected $article_model;
+  protected $category_model;
+  protected function _initialize()
+  {
+    parent::_initialize();
+    $this->article_model  = new ArticleModel();
+    $this->category_model = new CategoryModel();
+    $category_level_list = $this->category_model->getLevelList();
+    $this->assign('category_level_list', $category_level_list);
+  }
+  /**
+   * 文章管理
+   * @param int    $cid     分类ID
+   * @param string $keyword 关键词
+   * @param int    $page
+   * @return mixed
+   */
+  public function index($cid = 0, $keyword = '', $page = 1)
+  {
+    $map   = [];
+    $field = 'id,title,cid,author,reading,status,publish_time,sort';
+    if ($cid > 0) {
+      // 根据父级栏目ID，查询子级栏目ID ，数组形式
+      $category_children_ids = $this->category_model->where(['path' => ['like', "%,{$cid},%"]])->column('id');
 
-    protected function _initialize()
-    {
-        parent::_initialize();
-        $this->article_model  = new ArticleModel();
-        $this->category_model = new CategoryModel();
 
-        $category_level_list = $this->category_model->getLevelList();
-        $this->assign('category_level_list', $category_level_list);
+      // 将子栏目ID数组 转换成 字符串 ‘,’分隔，父栏目ID在最后
+      $category_children_ids = (!empty($category_children_ids) && is_array($category_children_ids)) ? implode(',', $category_children_ids) . ',' . $cid : $cid;
+      $map['cid']            = ['IN', $category_children_ids];
     }
-
-    /**
-     * 文章管理
-     * @param int    $cid     分类ID
-     * @param string $keyword 关键词
-     * @param int    $page
-     * @return mixed
-     */
-    public function index($cid = 0, $keyword = '', $page = 1)
-    {
-        $map   = [];
-        $field = 'id,title,cid,author,reading,status,publish_time,sort';
-
-        if ($cid > 0) {
-            $category_children_ids = $this->category_model->where(['path' => ['like', "%,{$cid},%"]])->column('id');
-            $category_children_ids = (!empty($category_children_ids) && is_array($category_children_ids)) ? implode(',', $category_children_ids) . ',' . $cid : $cid;
-            $map['cid']            = ['IN', $category_children_ids];
-        }
-
-        if (!empty($keyword)) {
-            $map['title'] = ['like', "%{$keyword}%"];
-        }
-
-        $article_list  = $this->article_model->field($field)->where($map)->order(['publish_time' => 'DESC'])->paginate(15, false, ['page' => $page]);
-        $category_list = $this->category_model->column('name', 'id');
-
-        return $this->fetch('index', ['article_list' => $article_list, 'category_list' => $category_list, 'cid' => $cid, 'keyword' => $keyword]);
+    if (!empty($keyword)) {
+      $map['title'] = ['like', "%{$keyword}%"];
     }
-
-    /**
-     * 添加文章
-     * @return mixed
-     */
-    public function add()
-    {
-        return $this->fetch();
-    }
-
-    /**
-     * 保存文章
-     */
-    public function save()
-    {
-        if ($this->request->isPost()) {
-            $data            = $this->request->param();
-            $validate_result = $this->validate($data, 'Article');
-
-            if ($validate_result !== true) {
-                $this->error($validate_result);
-            } else {
-                if ($this->article_model->allowField(true)->save($data)) {
-                    $this->success('保存成功');
-                } else {
-                    $this->error('保存失败');
-                }
-            }
-        }
-    }
-
-    /**
-     * 编辑文章
-     * @param $id
-     * @return mixed
-     */
-    public function edit($id)
-    {
-        $article = $this->article_model->find($id);
-
-        return $this->fetch('edit', ['article' => $article]);
-    }
-
-    /**
-     * 更新文章
-     * @param $id
-     */
-    public function update($id)
-    {
-        if ($this->request->isPost()) {
-            $data            = $this->request->param();
-            $validate_result = $this->validate($data, 'Article');
-
-            if ($validate_result !== true) {
-                $this->error($validate_result);
-            } else {
-                if ($this->article_model->allowField(true)->save($data, $id) !== false) {
-                    $this->success('更新成功');
-                } else {
-                    $this->error('更新失败');
-                }
-            }
-        }
-    }
-
-    /**
-     * 删除文章
-     * @param int   $id
-     * @param array $ids
-     */
-    public function delete($id = 0, $ids = [])
-    {
-        $id = $ids ? $ids : $id;
-        if ($id) {
-            if ($this->article_model->destroy($id)) {
-                $this->success('删除成功');
-            } else {
-                $this->error('删除失败');
-            }
+    $article_list  = $this->article_model->field($field)->where($map)->order(['publish_time' => 'DESC'])->paginate(15, false, ['page' => $page]);
+    $category_list = $this->category_model->column('name', 'id');
+    return $this->fetch('index', ['article_list' => $article_list, 'category_list' => $category_list, 'cid' => $cid, 'keyword' => $keyword]);
+  }
+  /**
+   * 添加文章
+   * @return mixed
+   */
+  public function add()
+  {
+    return $this->fetch();
+  }
+  /**
+   * 保存文章
+   */
+  public function save()
+  {
+    if ($this->request->isPost()) {
+      $data            = $this->request->param();
+      $validate_result = $this->validate($data, 'Article');
+      if ($validate_result !== true) {
+        $this->error($validate_result);
+      } else {
+        if ($this->article_model->allowField(true)->save($data)) {
+          $this->success('保存成功');
         } else {
-            $this->error('请选择需要删除的文章');
+          $this->error('保存失败');
         }
+      }
     }
-
-    /**
-     * 文章审核状态切换
-     * @param array  $ids
-     * @param string $type 操作类型
-     */
-    public function toggle($ids = [], $type = '')
-    {
-        $data   = [];
-        $status = $type == 'audit' ? 1 : 0;
-
-        if (!empty($ids)) {
-            foreach ($ids as $value) {
-                $data[] = ['id' => $value, 'status' => $status];
-            }
-            if ($this->article_model->saveAll($data)) {
-                $this->success('操作成功');
-            } else {
-                $this->error('操作失败');
-            }
+  }
+  /**
+   * 编辑文章
+   * @param $id
+   * @return mixed
+   */
+  public function edit($id)
+  {
+    $article = $this->article_model->find($id);
+    return $this->fetch('edit', ['article' => $article]);
+  }
+  /**
+   * 更新文章
+   * @param $id
+   */
+  public function update($id)
+  {
+    if ($this->request->isPost()) {
+      $data            = $this->request->param();
+      $validate_result = $this->validate($data, 'Article');
+      if ($validate_result !== true) {
+        $this->error($validate_result);
+      } else {
+        if ($this->article_model->allowField(true)->save($data, $id) !== false) {
+          $this->success('更新成功');
         } else {
-            $this->error('请选择需要操作的文章');
+          $this->error('更新失败');
         }
+      }
     }
+  }
+  /**
+   * 删除文章
+   * @param int   $id
+   * @param array $ids
+   */
+  public function delete($id = 0, $ids = [])
+  {
+    $id = $ids ? $ids : $id;
+    if ($id) {
+      if ($this->article_model->destroy($id)) {
+        $this->success('删除成功');
+      } else {
+        $this->error('删除失败');
+      }
+    } else {
+      $this->error('请选择需要删除的文章');
+    }
+  }
+  /**
+   * 文章审核状态切换
+   * @param array  $ids
+   * @param string $type 操作类型
+   */
+  public function toggle($ids = [], $type = '')
+  {
+    $data   = [];
+    $status = $type == 'audit' ? 1 : 0;
+    if (!empty($ids)) {
+      foreach ($ids as $value) {
+        $data[] = ['id' => $value, 'status' => $status];
+      }
+      if ($this->article_model->saveAll($data)) {
+        $this->success('操作成功');
+      } else {
+        $this->error('操作失败');
+      }
+    } else {
+      $this->error('请选择需要操作的文章');
+    }
+  }
 }
